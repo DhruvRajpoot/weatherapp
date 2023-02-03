cace69c8ff5e1818302e66994c18306562801f34
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-02-03 07:27:25 +0530

    fix: error on search

--- src/context/WeatherContext.js
+++ src/context/WeatherContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const WeatherContext = createContext()
@@ -8,51 +9,58 @@ export const WeatherProvider = ({ children }) => {
     const [lon, setLon] = useState('')
     const [city, setCity] = useState('Bhopal')
     const [state, setState] = useState('Madhya Pradesh')
-    const [weatherData,setWeatherData]=useState({})
+    const [weatherData, setWeatherData] = useState({})
 
     const findLatLon = async (city) => {
         let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
-        const response = await fetch(url);
-        const data = await response.json()
-        setLat(data[0].lat);
-        setLon(data[0].lon);
-        setCity(data[0].name)
-        setState(data[0].state)
+        await axios.get(url).then((response) => {
+            if (response.data.length !== 0) {
+                setLat(response.data[0].lat);
+                setLon(response.data[0].lon);
+                setCity(response.data[0].name);
+                setState(response.data[0].state);
+            }
+            else{
+                alert('City not found, Try to search with correct name')
+            }
+        })
     }
 
     const findWeather = async () => {
         let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
-        const response = await fetch(url);
-        const data = await response.json()
-        console.log(data)
-        setWeatherData(data)
+        await axios.get(url).then((response) => {
+            setWeatherData(response.data)
+        }).catch((err) => {
+            console.log(err.message)
+        })
     }
 
-    useEffect(()=>{
-        findWeather()
-    },[lat])
-
-    useEffect(()=>{
-        findLatLon(city)
-    },[])
-
-    const reverseGeoCode=async(latitude,longitude)=>{
-        let url=`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
-        const response = await fetch(url);
-        const data=await response.json()
-        setCity(data.city)
-        setState(data.principalSubdivision)
-        setLat(latitude);
-        setLon(longitude);
+    const reverseGeoCode = async (latitude, longitude) => {
+        let url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+        await axios.get(url).then((response) => {
+            setCity(response.data.city)
+            setState(response.data.principalSubdivision)
+            setLat(response.data.latitude);
+            setLon(response.data.longitude);
+        }).catch((err) => {
+            console.log(err.message)
+        })
     }
 
     useEffect(() => {
-        console.log(weatherData);
+        findWeather()
+    }, [lat])
+
+    useEffect(() => {
+        findLatLon(city)
+    }, [])
+
+    useEffect(() => {
         navigator.geolocation.getCurrentPosition((success) => {
             let { latitude, longitude } = success.coords;
-            reverseGeoCode(latitude,longitude)
+            reverseGeoCode(latitude, longitude)
         }, (err) => {
-            // findLatLon(city)
+            findLatLon(city)
         })
     }, [])
 
